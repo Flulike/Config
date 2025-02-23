1eee5144af66938aa45bb3a5721aa77b97b74ff5
Fluike <gyp12138@163.com>
2025-02-23 15:59:12 +0900

    test

--- ultralytics/train/train_detr.py
+++ ultralytics/train/train_detr.py
@@ -4,8 +4,8 @@ device = 2
 
 # Load a model
 model = RTDETR('/home/guo/ultralytics/ultralytics/cfg/models/uva_detr/uvadetr-r50.yaml')  # build a new model from YAML
-# model = RTDETR("rtdetr-l.pt")  # load a pretrained model (recommended for training)
-# model = RTDETR("rtdetr-l.yaml").load("rtdetr-l.pt")  # build from YAML and transfer weights
+model = RTDETR("rtdetr-l.pt")  # load a pretrained model (recommended for training)
+model = RTDETR("rtdetr-l.yaml").load("rtdetr-l.pt")  # build from YAML and transfer weights
 
 project = "/home/guo/results/uva_rtdetr/car"
 
